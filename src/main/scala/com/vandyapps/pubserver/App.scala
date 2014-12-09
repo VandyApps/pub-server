c5eec5451cabe165410a0d521158a50909bc2f9f
AliceCengal <cengallut@gmail.com>
2014-12-09 12:00:06 -0600

    Extracted the controller

--- src/main/scala/com/vandyapps/pubserver/App.scala
+++ src/main/scala/com/vandyapps/pubserver/App.scala
@@ -8,52 +8,7 @@ object PubApp extends App with RequestValidation {
   println("================================")
   println("=          PUB SERVER          =")
   println("================================")
-  
-
-  class MainController extends Controller
-      with OrderRegister {
-
-    get("/") { request =>
-      render.static("index.txt").toFuture
-    }
-
-    get("/order") { request =>
-      (for {
-        countStr <- request.params.get("count");
-        count    <- Try(countStr.toInt).toOption
-      } yield PubReport(status = "Okay",
-                        orders = getOrders(count)))
-          .getOrElse(PubReport(status = "Malformed request"))
-          .>>>(render.json(_).toFuture)
-    }
-
-    post("/order") { request =>
-      (for {
-        orderStr <- request.params.get("orderNumber");
-        apikey   <- request.params.get("apikey");
-        orderNum <- Try(orderStr.toInt).toOption;
-        if isValid(apikey, orderNum)
-      } yield orderNum)
-          .map { num =>
-              addOrder(num)
-              PubReport(status = "Successfully added order #" + num) }
-          .getOrElse(PubReport(status = "Malformed request"))
-          .>>>(render.json(_).toFuture)
-    }
-
-    get("/console") { request =>
-      render.static("main.html").toFuture
-    }
-
-    notFound { request =>
-      render.json(PubReport(status = "Wrong API call")).toFuture
-    }
-
-    error { request =>
-      render.json(PubReport(status = s"Something's wrong: ${request.error}")).toFuture
-    }
-
-  }
+  println()
 
   this.args match {
     case Array("help")    => println(usageGuide)
@@ -72,13 +27,11 @@ object PubApp extends App with RequestValidation {
     println(s"With password: ${apiKey}")
     
     val server = new FinatraServer
-    server.register(new MainController)
+    server.register(new MainController(this))
     server.start()
   }
 
-  implicit class Piped[T](pipee: T) {
-    def >>>[U](receiver: T=>U): U = receiver(pipee)
-  }
+  
 
   lazy val apiKey = ""
   
@@ -141,4 +94,57 @@ trait RequestValidation {
   private def toHex(bytes: Array[Byte]) =
     javax.xml.bind.DatatypeConverter.printHexBinary(bytes)
 
+}
+
+class MainController(validator: RequestValidation) extends Controller
+    with OrderRegister {
+  import MainController._
+  import validator._
+
+  get("/") { request =>
+    render.static("index.txt").toFuture
+  }
+
+  get("/order") { request =>
+    (for {
+      countStr <- request.params.get("count");
+      count    <- Try(countStr.toInt).toOption
+    } yield PubReport(status = "Okay",
+                      orders = getOrders(count)))
+        .getOrElse(PubReport(status = "Malformed request"))
+        .>>>(render.json(_).toFuture)
+  }
+
+  post("/order") { request =>
+    (for {
+      orderStr <- request.params.get("orderNumber");
+      apikey   <- request.params.get("apikey");
+      orderNum <- Try(orderStr.toInt).toOption;
+      if isValid(apikey, orderNum)
+    } yield orderNum)
+        .map { num =>
+            addOrder(num)
+            PubReport(status = "Successfully added order #" + num) }
+        .getOrElse(PubReport(status = "Malformed request"))
+        .>>>(render.json(_).toFuture)
+  }
+
+  get("/console") { request =>
+    render.static("main.html").toFuture
+  }
+
+  notFound { request =>
+    render.json(PubReport(status = "Wrong API call")).toFuture
+  }
+
+  error { request =>
+    render.json(PubReport(status = s"Something's wrong: ${request.error}")).toFuture
+  }
+
+}
+
+object MainController {
+  implicit class Piped[T](pipee: T) {
+    def >>>[U](receiver: T=>U): U = receiver(pipee)
+  }
 }
